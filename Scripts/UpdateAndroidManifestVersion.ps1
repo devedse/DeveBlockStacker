199c69a2cf924c1d37d054db630fc9007029b7e0
Devedse <davydavidse@gmail.com>
2018-11-21 21:06:06 +0100

    Fixed path

--- Scripts/UpdateAndroidManifestVersion.ps1
+++ Scripts/UpdateAndroidManifestVersion.ps1
@@ -11,15 +11,17 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "Testje: $($env.APPVEYOR_BUILD_VERSION)  en $($env.APPVEYOR_BUILD_ID) en $($env.APPVEYOR_BUILD_NUMBER)"
 
-Write-Host "Updating AndroidManifest file: $androidManifestPath"
+$fullAndroidManifestPath = (Resolve-Path $androidManifestPath).Path
+
+Write-Host "Updating AndroidManifest file: $fullAndroidManifestPath"
 Write-Host "android:versionName: $version"
 Write-Host "android:versionCode: $buildNumber"
 
 #$ManifestFile = Get-ChildItem -Path $pwd -Filter AndroidManifest.xml -Recurse
-$fileXml = [xml] (Get-Content $androidManifestPath )
+$fileXml = [xml] (Get-Content $fullAndroidManifestPath )
 $xpath = "//manifest"
 Select-Xml -xml $fileXml -XPath $xpath | %{
     $_.Node.SetAttribute("android:versionName", $version)
     $_.Node.SetAttribute("android:versionCode", $buildNumber)
 }
-$fileXml.Save($androidManifestPath)
+$fileXml.Save($fullAndroidManifestPath)
