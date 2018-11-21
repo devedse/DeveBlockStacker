77f388ee033d087fff5183f4b201ede3e4da049c
Devedse <davydavidse@gmail.com>
2018-11-22 00:09:00 +0100

    Renamed to UWP

--- DeveBlockStacker.UWP/GamePage.xaml.cs
+++ DeveBlockStacker.UWP/GamePage.xaml.cs
@@ -4,7 +4,7 @@ using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace DeveBlockStacker.Windows10Universal
+namespace DeveBlockStacker.UWP
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
--- DeveBlockStacker.UWP/Properties/AssemblyInfo.cs
+++ DeveBlockStacker.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeveBlockStacker.Windows10Universal")]
+[assembly: AssemblyTitle("DeveBlockStacker.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeveBlockStacker.Windows10Universal")]
+[assembly: AssemblyProduct("DeveBlockStacker.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
