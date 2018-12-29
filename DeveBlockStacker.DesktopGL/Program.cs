569d50461765731db94ab278d3ff767e6164065a
Devedse <davydavidse@gmail.com>
2018-12-29 17:08:32 +0100

    Added DesktopGL and Windows project

---
+++ DeveBlockStacker.DesktopGL/Program.cs
@@ -0,0 +1,23 @@
+﻿using DeveBlockStacker.Shared;
+using System;
+
+namespace DeveBlockStacker.DesktopGL
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new TheGame(720 / 2, 1280 / 2))
+            {
+                game.Run();
+            }
+        }
+    }
+}
