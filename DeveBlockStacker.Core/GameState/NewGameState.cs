66691a2d9553879835b2faa25ce87527fa7c139d
Devedse <davydavidse@gmail.com>
2022-03-03 20:45:40 +0100

    Added version in startup screen

--- DeveBlockStacker.Core/GameState/NewGameState.cs
+++ DeveBlockStacker.Core/GameState/NewGameState.cs
@@ -14,7 +14,10 @@ namespace DeveBlockStacker.Core.GameState
         private static readonly string n = Environment.NewLine;
 
         private readonly string helloString = $"Hello{n}Welcome to the best game EU!{n}My friend says this game is 5 out of 7!{n}{n}I stopped playing WoW for this game{n}#Gamermagazine2019{n}{n}Haven't been outside for 2 years since the release{n}#TheRealTrump{n}{n}I kinda miss my dog :'({n}#Keanu{n}{n}Made by Devedse";
-        private Vector2 measuredString;
+        private Vector2 measuredHelloString;
+
+        private readonly string versionString = $"Version: {typeof(NewGameState).Assembly.GetName().Version}";
+        private Vector2 measuredVersionString;
 
         public NewGameState()
         {
@@ -33,16 +36,26 @@ namespace DeveBlockStacker.Core.GameState
 
         public void Draw(SpriteBatch spriteBatch, ContentDistributionThing contentDistributionThing)
         {
-            if (measuredString == Vector2.Zero)
+            if (measuredHelloString == Vector2.Zero)
             {
-                measuredString = contentDistributionThing.SecularOne72.MeasureString(helloString);
+                measuredHelloString = contentDistributionThing.SecularOne72.MeasureString(helloString);
+            }
+            if (measuredVersionString == Vector2.Zero)
+            {
+                measuredVersionString = contentDistributionThing.SecularOne72.MeasureString(versionString);
             }
 
+            var posHelloString = new Vector2(contentDistributionThing.ScreenWidth / 2, contentDistributionThing.ScreenHeight / 2 - contentDistributionThing.ScreenHeight / 10f + (contentDistributionThing.ScreenHeight / 5.0f * (float)Math.Sin(framesDelay / 20.0f)));
+            var scaleHelloString = contentDistributionThing.ScreenWidth / (measuredHelloString.X * 1.3f);
 
-            var pos = new Vector2(contentDistributionThing.ScreenWidth / 2, contentDistributionThing.ScreenHeight / 2 - contentDistributionThing.ScreenHeight / 10f + (contentDistributionThing.ScreenHeight / 5.0f * (float)Math.Sin(framesDelay / 20.0f)));
-            var scale = contentDistributionThing.ScreenWidth / (measuredString.X * 1.3f);
+            //Take up 50% of the screen (in width) (also we use {sideDistance} to move it away from the side)
+            int sideDistance = 5;
+            var scaleVersionString = (1f / ((measuredVersionString.X + sideDistance) / contentDistributionThing.ScreenWidth)) * 0.25f;
+            var scaledMeasuredString = measuredVersionString * scaleVersionString;
+            var posVersionString = new Vector2(contentDistributionThing.ScreenWidth - scaledMeasuredString.X - sideDistance, contentDistributionThing.ScreenHeight - scaledMeasuredString.Y - sideDistance);
 
-            spriteBatch.DrawString(contentDistributionThing.SecularOne72, helloString, pos, Color.White, 0, measuredString / 2, scale, SpriteEffects.None, 0);
+            spriteBatch.DrawString(contentDistributionThing.SecularOne72, helloString, posHelloString, Color.White, 0, measuredHelloString / 2, scaleHelloString, SpriteEffects.None, 0);
+            spriteBatch.DrawString(contentDistributionThing.SecularOne72, versionString, posVersionString, Color.White, 0, Vector2.Zero, scaleVersionString, SpriteEffects.None, 0);
         }
     }
 }
