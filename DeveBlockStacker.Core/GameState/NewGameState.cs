342da99f5d8a3a583eb9b237c8b4cc4e930ec5f5
Devedse <davydavidse@gmail.com>
2022-04-06 01:57:34 +0200

    Added some information on the current resolution

--- DeveBlockStacker.Core/GameState/NewGameState.cs
+++ DeveBlockStacker.Core/GameState/NewGameState.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using DeveBlockStacker.Core.HelperObjects;
 
 namespace DeveBlockStacker.Core.GameState
 {
@@ -13,19 +14,22 @@ namespace DeveBlockStacker.Core.GameState
 
         private static readonly string n = Environment.NewLine;
 
-        private readonly string helloString = $"Hello{n}Welcome to the best game EU!{n}My friend says this game is 5 out of 7!{n}{n}I stopped playing WoW for this game{n}#Gamermagazine2019{n}{n}Haven't been outside for 2 years since the release{n}#TheRealTrump{n}{n}I kinda miss my dog :'({n}#Keanu{n}{n}Made by Devedse";
+        private readonly string _helloString = $"Hello{n}Welcome to the best game EU!{n}My friend says this game is 5 out of 7!{n}{n}I stopped playing WoW for this game{n}#Gamermagazine2019{n}{n}Haven't been outside for 2 years since the release{n}#TheRealTrump{n}{n}I kinda miss my dog :'({n}#Keanu{n}{n}Made by Devedse";
         private Vector2 measuredHelloString;
 
 #if NETFRAMEWORK
-        private readonly string versionString = $"Version: _web_";
+        private readonly string version = "_web_";
 #else
-        private readonly string versionString = $"Version: {typeof(NewGameState).Assembly.GetName().Version}";
+        private readonly string version = typeof(NewGameState).Assembly.GetName().Version.ToString();
 #endif
-        private Vector2 measuredVersionString;
+        private string _versionString = "";
+        private Vector2 _measuredVersionString;
+        public IntSize? _lastScreenSizeTotal = null;
+        public Rectangle? _lastScreenSizeGame = null;
 
         public NewGameState()
         {
-            framesDelay = 400;
+            framesDelay = 4000;
         }
 
         public IGameState Update(InputStatifier inputStatifier)
@@ -40,15 +44,19 @@ namespace DeveBlockStacker.Core.GameState
 
         public void Draw(SpriteBatch spriteBatch, ContentDistributionThing contentDistributionThing)
         {
+            var totSize = contentDistributionThing.ScreenSizeTotal;
             var gameSize = contentDistributionThing.ScreenSizeGame;
 
             if (measuredHelloString == Vector2.Zero)
             {
-                measuredHelloString = contentDistributionThing.SecularOne72.MeasureString(helloString);
+                measuredHelloString = contentDistributionThing.SecularOne72.MeasureString(_helloString);
             }
-            if (measuredVersionString == Vector2.Zero)
+
+            var newVersionString = $"ScreenSize: {totSize.Width}x{totSize.Height}{n}GameSize: {gameSize.Width}x{gameSize.Height}{n}Version: {version}";
+            if (_measuredVersionString == Vector2.Zero || newVersionString != _versionString)
             {
-                measuredVersionString = contentDistributionThing.SecularOne72.MeasureString(versionString);
+                _versionString = newVersionString;
+                _measuredVersionString = contentDistributionThing.SecularOne72.MeasureString(_versionString);
             }
 
             var posHelloString = new Vector2(gameSize.Width / 2, gameSize.Height / 2 - gameSize.Height / 10f + (gameSize.Height / 5.0f * (float)Math.Sin(framesDelay / 20.0f)));
@@ -56,12 +64,12 @@ namespace DeveBlockStacker.Core.GameState
 
             //Take up 50% of the screen (in width) (also we use {sideDistance} to move it away from the side)
             int sideDistance = 5;
-            var scaleVersionString = (1f / ((measuredVersionString.X + sideDistance) / gameSize.Width)) * 0.25f;
-            var scaledMeasuredString = measuredVersionString * scaleVersionString;
+            var scaleVersionString = (1f / ((_measuredVersionString.X + sideDistance) / gameSize.Width)) * 0.25f;
+            var scaledMeasuredString = _measuredVersionString * scaleVersionString;
             var posVersionString = new Vector2(gameSize.Width - scaledMeasuredString.X - sideDistance, gameSize.Height - scaledMeasuredString.Y - sideDistance);
 
-            spriteBatch.DrawString(contentDistributionThing.SecularOne72, helloString, posHelloString, Color.White, 0, measuredHelloString / 2, scaleHelloString, SpriteEffects.None, 0);
-            spriteBatch.DrawString(contentDistributionThing.SecularOne72, versionString, posVersionString, Color.White, 0, Vector2.Zero, scaleVersionString, SpriteEffects.None, 0);
+            spriteBatch.DrawString(contentDistributionThing.SecularOne72, _helloString, posHelloString, Color.White, 0, measuredHelloString / 2, scaleHelloString, SpriteEffects.None, 0);
+            spriteBatch.DrawString(contentDistributionThing.SecularOne72, _versionString, posVersionString, Color.White, 0, Vector2.Zero, scaleVersionString, SpriteEffects.None, 0);
         }
     }
 }
--- DeveBlockStacker.Core/HelperObjects/IntSize.cs
+++ DeveBlockStacker.Core/HelperObjects/IntSize.cs
@@ -1,6 +1,8 @@
-﻿namespace DeveBlockStacker.Core.HelperObjects
+﻿using System;
+
+namespace DeveBlockStacker.Core.HelperObjects
 {
-    public struct IntSize
+    public struct IntSize : IEquatable<IntSize>
     {
         public int Width { get; }
         public int Height { get; }
@@ -8,6 +10,35 @@
         {
             Width = width;
             Height = height;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is IntSize size && Equals(size);
+        }
+
+        public bool Equals(IntSize other)
+        {
+            return Width == other.Width &&
+                   Height == other.Height;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 859600377;
+            hashCode = hashCode * -1521134295 + Width.GetHashCode();
+            hashCode = hashCode * -1521134295 + Height.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(IntSize left, IntSize right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(IntSize left, IntSize right)
+        {
+            return !(left == right);
         }
     }
 }
